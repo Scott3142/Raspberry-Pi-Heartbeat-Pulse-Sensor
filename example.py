d3ac03e63c25bf4babac5f0a3ced64c4b8082e11
Scott Morgan <smorgan@bridgend.ac.uk>
2020-03-05 16:10:47 +0000

    Adds comments

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
-from pulsesensor import Pulsesensor
-import time
+from pulsesensor import Pulsesensor #import pulse sensor library
+import time #import time library
 
-p = Pulsesensor()
-p.startAsyncBPM()
+p = Pulsesensor() #store pulse sensor in variable p (tidies code up)
+p.startAsyncBPM() #starts the BPM (beats per minute) monitoring
 
-try:
-    while True:
-        bpm = p.BPM
-        if bpm > 0:
-            print("BPM: %d" % bpm)
-        else:
-            print("No Heartbeat found")
-        time.sleep(1)
+try: #allows you to cancel with Ctrl-C
+    while True: #loops forever
+        bpm = p.BPM #stores BPM in bpm variable
+        if bpm > 0: #checks for pulse
+            print("BPM: " + str(bpm)) #this prints the BPM to screen
+        else: #this happens if no beats per minute register
+            print("No Heartbeat found") 
+        time.sleep(1) #waits for 1 second
 except:
-    p.stopAsyncBPM()
+    p.stopAsyncBPM() #stops BPM monitoring if Ctrl-C is pressed
